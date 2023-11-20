3443b173b7053582632df3b2ba2767205f2cc225
VIKAS <123455372+VIKAS-cybercode@users.noreply.github.com>
2023-11-20 17:06:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ var http=require("http").Server(app);
 var io=require("socket.io")(http);
 const express=require("express");
 app.use(express.json());
-const collection=require("./mongodb")
-
+const {collection,profile}=require("./mongodb")
 
 var user_n="";
 app.get("/",function(req,res){
@@ -64,12 +63,20 @@ app.get('/login.html',function(req,res){
  app.get('/profile-style.css',function(req,res){
    res.sendFile(__dirname+"/profile-style.css");
  })
+ app.get('/tick_tick.mp3',function(req,res){
+   res.sendFile(_dirname+"/tick_tick.mp3");
+ })
+ app.get('/time_up.mp3',function(req,res){
+   res.sendFile(_dirname+"/time_up.mp3");
+ })
+
 
 
 users=[];
 broadcastt=[];
 SID=[];
 let myMap1=new Map();
+let myMap2=new Map();
 arr1=['apple', 'banana', 'car', 'dog', 'elephant', 'fan', 'grape', 'hat', 'ice cream', 'jacket', 'kettle', 'lamp', 'mango', 'notebook', 'orange', 'pencil', 'quilt', 'rabbit', 'spoon', 'table', 'umbrella', 'vase', 'watermelon', 'xylophone', 'yacht', 'zebra', 'ant', 'ball', 'cat', 'duck', 'eagle', 'fish', 'giraffe', 'horse', 'iguana', 'jackal', 'kangaroo', 'lion', 'monkey', 'nest', 'owl', 'parrot', 'quail', 'rat', 'snake', 'tiger', 'unicorn', 'vulture', 'wolf', 'x-ray fish', 'yak', 'zebu', 'airplane', 'boat', 'cycle', 'drum', 'earphone', 'flute', 'guitar', 'harmonica', 'ipad', 'juicer', 'keyboard', 'laptop', 'mobile', 'nail cutter', 'oven', 'piano', 'quiver', 'ruler', 'scissors', 'television', 'utensils', 'violin', 'washing machine', 'xerox machine', 'yoyo', 'zipper', 'alarm clock', 'basket', 'candle', 'diary', 'eraser', 'fork', 'glass', 'hammer', 'ink', 'jug', 'knife', 'lock', 'mirror', 'needle', 'oil can', 'pillow', 'quartz', 'rose', 'soap', 'toothbrush', 'umbrella', 'violin', 'wallet', 'xmas tree', 'yogurt', 'zip'];
 guessed_users=[];
 var user_id;
@@ -100,6 +107,7 @@ socket.on('feed-d',function(data){
    SID.push(data.Sid);
    console.log("in SID"+data.Sid);
    myMap1.set(data.Sid,data.U);
+   myMap2.set(data.U,0);
 })
   
  socket.on('setUsername',function(data){
@@ -116,7 +124,6 @@ socket.on('feed-d',function(data){
     }
  })
  socket.on('setL',function(data){
-   console.log(data+"*********");
    if(broadcastt.indexOf(data)<0){
     socket.broadcast.emit('setLead',data)
     broadcastt.push(data);
@@ -147,6 +154,8 @@ socket.on('giveWordC',function(data){
       random_word_sign+="_ ";
    }
    io.to(SID[data.User-1]).emit('giveWordS',random_word);
+   io.to(SID[data.User-1]).emit('givepermissionT');
+
    var socket_t = io.sockets.sockets.get(SID[data.User-1]);
    socket_t.broadcast.emit('giveWordS',random_word_sign);
 
@@ -209,13 +218,65 @@ socket.on('give_scoreC',function(){
    }
    guessed_users.length=0;
 })
+socket.on('givepermissionNTA',function(){
+   console.log("yyyuta");
+   io.sockets.emit('givepermissionNT');
+})
 //---------------------------profile page socket work------------------------------------------//
-socket.on('getname',function(){
-   console.log(user_id+"*");
-   socket.emit('setname',user_id);
+socket.on('getprofiledata', async function () {
+   console.log(user_id + "*");
+   try {
+     const profiled = await profile.findOne({ name: user_id });
+     const lgame=profiled.last3game.lastgame;
+     const slgame=profiled.last3game.slastgame;
+     const tgame=profiled.last3game.tlastgame;
+     const lgp=profiled.last3position.lastposition;
+     const sgp=profiled.last3position.slastposition;
+     const tgp=profiled.last3position.tlastposition;
+
+     socket.emit('setprofiledata', { name: user_id, GP: profiled.TotalGP, HS: profiled.HighestScore,lgame:lgame,slgame:slgame,tgame:tgame,lgp:lgp,sgp:sgp,tgp:tgp});
+   } catch (error) {
+     console.error(error);
+   }
+ });
+//---------------------------profile page data updation--------------------------//
+socket.on('updateScore',function(data){
+   myMap2.set(data.u,data.value);
 })
 socket.on('id-name',function(data){
    users.push(data);
+})
+socket.on('updateprofile',async function(data){
+   for(let i=0;i<users.length;i++){
+      const u = await profile.findOne({ name:users[i] });
+      if(u){
+         var totalgame=u.TotalGP+1;
+         console.log(totalgame);
+         await profile.updateOne(
+            { name: users[i] },
+            { $set: { 'TotalGP': totalgame } }
+          );
+          var highscore=u.HighestScore;
+          if(Number(myMap2.get(u.name))>highscore){
+            highscore=Number(myMap2.get(u.name));
+          }
+          await profile.updateOne(
+            { name: users[i] },
+            { $set: { 'HighestScore': highscore } }
+          );
+         var lastgamescore=u.last3game.lastgame;
+         var slastgamescore=u.last3game.slastgame;
+         await profile.updateOne(
+            {name:users[i]},
+            {$set:{last3game:{
+               lastgame:Number(myMap2.get(u.name)),
+               slastgame:lastgamescore,
+               tlastgame:slastgamescore
+            }}}
+         ) 
+
+      }
+   }
 })
 });
 
@@ -225,7 +286,24 @@ app.post("/signup", async (req, res) => {
      name: req.body.name,
      password: req.body.password,
    };
+   const profileInitialise={
+      name:req.body.name,
+      TotalGP:0,
+      HighestScore:0,
+      last3game:{
+         lastgame:0,
+         slastgame:0,
+         tlastgame:0,
+      },
+      last3position:{
+         lastposition:0,
+         slastposition:0,
+         tlastposition:0,
+
+      }
+   };
    await collection.insertMany([data]);
+   await profile.insertMany([profileInitialise]);
    res.redirect("/login.html");
  });
  
@@ -242,6 +320,7 @@ app.post("/signup", async (req, res) => {
      res.send("Error in login. Please try again.");
    }
  });
+//profile page database_______________________
 
 
 http.listen(3212,function(){
