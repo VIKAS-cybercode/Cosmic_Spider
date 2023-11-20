96fa15ae46cbd588c18e35cfd18a1b87099a4bbe
VIKAS <123455372+VIKAS-cybercode@users.noreply.github.com>
2023-11-20 17:10:36 +0530

    Update profile.js

--- profile.js
+++ profile.js
@@ -1,9 +1,19 @@
 var socket=io();
 var n;
-socket.emit('getname');
-socket.on('setname',function(data){
-    n=data;
-    document.getElementById("profile").innerHTML+=data;
+socket.emit('getprofiledata');
+socket.on('setprofiledata',function(data){
+    n=data.name;
+    console.log(data.GP);
+    document.getElementById("profile").innerHTML+=data.name;
+    document.getElementById("TGP").innerHTML+=data.GP;
+    document.getElementById("HS").innerHTML+=data.HS;
+    document.getElementById("lgp").innerHTML+=data.lgp;
+    document.getElementById("sgp").innerHTML+=data.sgp;
+    document.getElementById("tgp").innerHTML+=data.tgp;
+    document.getElementById("lgs").innerHTML+=data.lgame;
+    document.getElementById("sgs").innerHTML+=data.slgame;
+    document.getElementById("tgs").innerHTML+=data.tgame;
+    
 })
 function gameEnter(){
 localStorage.setItem('user-name', n);
