00fea4511377ee450b240bff2c1f7a1b49d14e1e
Aditya Parmar <125036952+aditya051102@users.noreply.github.com>
2023-11-21 22:32:29 +0530

    Update profile.js

--- profile.js
+++ profile.js
@@ -5,6 +5,7 @@ socket.on('setprofiledata',function(data){
     n=data.name;
     console.log(data.GP);
     document.getElementById("profile").innerHTML+=data.name;
+    document.getElementById("profileImage").src ="image"+data.imageNo+ ".jpg"; ;
     document.getElementById("TGP").innerHTML+=data.GP;
     document.getElementById("HS").innerHTML+=data.HS;
     document.getElementById("lgp").innerHTML+=data.lgp;
@@ -20,3 +21,21 @@ localStorage.setItem('user-name', n);
 socket.emit('id-name',n);
 window.location.href = "gamepage.html";
 }
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+  }
+  
+  function chooseImage(imageNo) {
+    // Replace the current profile image
+    document.getElementById("profileImage").src ="image"+imageNo+ ".jpg";
+    var user=n;
+    socket.emit("changepic",{user:user,img:imageNo})
+    closeForm();
+   
+  }
+  
+  
