7e593cc282df02f628e95e9612ad0d422d15005e
Mrinal Sharma <125037134+MrinalSharma87@users.noreply.github.com>
2023-11-24 00:33:51 +0530

    Update index.js

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,3 +1,33 @@
+
+// Get the pop-up
+var popUp = document.getElementById("myPopUp");
+
+// Get the button that opens the pop-up
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the pop-up
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the pop-up 
+btn.onclick = function () {
+    popUp.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the pop-up
+span.onclick = function () {
+    popUp.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the pop-up, close it
+window.onclick = function (event) {
+    if (event.target == popUp) {
+        popUp.style.display = "none";
+    }
+}
+
+
+
+
 var socket = io();
 function setUsername() {
     if (document.getElementById('name').value == '') {
@@ -78,3 +108,6 @@ canvas.addEventListener("mouseout", () => {
     isDrawing = false;
     clearCanvas();
 }); 
+
+
+
