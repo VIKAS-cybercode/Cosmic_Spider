741f9046f9b7cfdf0f35e816b6d800e14cef9393
VIKAS <123455372+VIKAS-cybercode@users.noreply.github.com>
2023-11-23 11:15:24 +0530

    Update profile.js

--- JavaScript/profile.js
+++ JavaScript/profile.js
@@ -1,24 +1,34 @@
 var socket=io();
-var n;
-socket.emit('getprofiledata');
-socket.on('setprofiledata',function(data){
-    n=data.name;
-    console.log(data.GP);
-    document.getElementById("profile").innerHTML+=data.name;
-    document.getElementById("profileImage").src ="/media/image"+data.imageNo+ ".jpg"; ;
-    document.getElementById("TGP").innerHTML+=data.GP;
-    document.getElementById("HS").innerHTML+=data.HS;
-    document.getElementById("lgp").innerHTML+=data.lgp;
-    document.getElementById("sgp").innerHTML+=data.sgp;
-    document.getElementById("tgp").innerHTML+=data.tgp;
-    document.getElementById("lgs").innerHTML+=data.lgame;
-    document.getElementById("sgs").innerHTML+=data.slgame;
-    document.getElementById("tgs").innerHTML+=data.tgame;
-    
-})
+var userr;
+document.addEventListener('DOMContentLoaded', () => {
+  // Fetch user data from the server
+  fetch('/getProfileData')
+      .then(response => response.json())
+      .then(data => {
+        userr=data.name;
+        console.log(data);
+        document.getElementById('profile').textContent = data.name;
+        document.getElementById("profileImage").src ="/media/image"+data.profileimg+ ".jpg"; ;
+        document.getElementById("TGP").innerHTML=data.TotalGP;
+        document.getElementById("HS").innerHTML=data.HighestScore;
+       const lgp=data.last3position.lastposition;
+       const sgp=data.last3position.slastposition;
+       const tgp=data.last3position.tlastposition;
+      document.getElementById("lgp").innerHTML=lgp;
+      document.getElementById("sgp").innerHTML=sgp;
+      document.getElementById("tgp").innerHTML=tgp;
+      const lgame=data.last3game.lastgame;
+       const slgame=data.last3game.slastgame;
+        const tlgame=data.last3game.tlastgame;
+
+       document.getElementById("lgs").innerHTML=lgame;
+       document.getElementById("sgs").innerHTML=slgame;
+       document.getElementById("tgs").innerHTML=tlgame;
+      });
+});
 function gameEnter(){
-localStorage.setItem('user-name', n);
-socket.emit('id-name',n);
+localStorage.setItem('user-name', userr);
+socket.emit('id-name',userr);
 window.location.href = "/HTML/gamepage.html";
 }
 function openForm() {
@@ -32,8 +42,22 @@ function openForm() {
   function chooseImage(imageNo) {
     // Replace the current profile image
     document.getElementById("profileImage").src ="/media/image"+imageNo+ ".jpg";
-    var user=n;
-    socket.emit("changepic",{user:user,img:imageNo})
+    var user=userr;
+    fetch('http://localhost:3212/update', {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        name:userr,
+        value:imageNo,
+      }),
+    })
+    .then(response => response.text())
+    .then(data => console.log(data))
+    .catch((error) => {
+      console.error('Error:', error);
+    });
     closeForm();
    
   }
